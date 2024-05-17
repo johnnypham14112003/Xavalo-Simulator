2d480baf3803654642fa28053c9826d76a06c727
thiennlse <thiennlse172858@gmail.com>
2024-05-17 08:36:20 +0700

    "Adding more character "

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehavior
+{
+    Vector2 movementInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void FixedUPdate()
+    {
+        if (movementInput != Vector2.) { }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+
+    }
+}
